d0f41699bd4f24cdaf0c0e377483530a8280e2ac
heeecker <89743412+filip326@users.noreply.github.com>
2023-06-24 07:59:07 -0700

    small change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ async function main() {
     app.use(express.static("public"));
 
     app.listen(process.env.PORT ?? 3000, () => {
-        console.log("INFO", `Server runs on localhost:${process.env.PORT}`);
+        console.log("INFO", `Server runs on localhost: ${process.env.PORT}`);
     });
 };
 
