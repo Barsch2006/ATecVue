c8aa9ff530dd1b774b899cac0f63482e5bf6e049
Barsch2006 <cchr98756@gmail.com>
2023-06-24 17:17:27 +0200

    add some Rules

--- frontend/src/pages/ATecRules.vue
+++ frontend/src/pages/ATecRules.vue
@@ -5,69 +5,88 @@ export default {
 </script>
 
 <template>
-    <v-expansion-panels>
-        <v-expansion-panel title="1. Einleitung">
+    <v-expansion-panels
+        title="Bestimmungen über die Verwaltung, Nutzung und Wartung der Aula und der entsprechenden Bühnentechnik, sowie über Veranstaltungen und deren Ablauf">
+        <v-expansion-panel title="1. Präambel">
             <v-expansion-panel-text>
-                
+                Die Technik AG des Gymnasiums Riedberg (weiter nur ATec) hilft der Schule bei der Repräsentation nach innen
+                und außen, bei der Durchführung von Veranstaltungen in und außerhalb der Aula und unterstützt die
+                Organisatoren einer Veranstaltung bei einem reibungslosen Ablauf durch Aufbau, Betrieb, Überwachung und
+                Abbau der benötigten Technik. In der Vergangenheit gab es immer wieder Unsicherheiten, Unklarheiten oder
+                Missverständnisse, die durch eine Vereinbarung zwischen der Schulleitung (im Folgenden nur SL) und der ATec
+                AG beseitigt und geklärt werden sollen. Diese Regelung soll alle Beteiligten nachhaltig entlasten, zu einem
+                reibungslosen Ablauf beitragen und die Aufgabenverteilung bei einer Veranstaltung klar regeln.
             </v-expansion-panel-text>
         </v-expansion-panel>
 
 
         <v-expansion-panel title="2. Geltungsbereich">
             <v-expansion-panel-text>
-                
+                Diese Bestimmungen sind nach der Unterschrift der Schulleitung ab sofort gültig. Die SL erklärt mit ihrer
+                Unterschrift der ATec ihr Einverständnis dieser Regelungen und verpflichtet sich, im Rahmen ihres Hausrechts
+                der Durchsetzung dieser Regelungen beizutragen. <br>
+                Betroffen von diesen Regelungen sind alle Mitglieder der Schulgemeinde. Auch betroffen sind alle externen
+                Personen, welche an einer Veranstaltung innerhalb der Schule teilnehmen oder eine organisieren. <br>
+                Diese Regeln behalten auf unbegrenzte Zeit ihre Gültigkeit.
             </v-expansion-panel-text>
         </v-expansion-panel>
 
         <v-expansion-panel title="3. Rechte der ATec">
             <v-expansion-panel-text>
-
+                Die ATec Techniker haben innerhalb der Aula sowie bei Veranstaltungen auf dem Schulgelände innerhalb des
+                Veranstaltungsbereichs ein Weisungsrecht. Anweisungen dürfen nicht willkürlich sein. Die Lehrkräfte und die
+                Schulleitung unterstützen die ATec Techniker beim Durchsetzten von der ATec getroffener Entscheidungen. Das
+                Weisungsrecht beinhaltet nach einer Verwarnung auch den Verweis des Veranstaltungsortes oder der Aula.
+                Dieser darf ebenfalls nicht willkürlich sein, und nur in begründeten Fällen nach Ermahnung der verwiesenen
+                Person ausgesprochen werden oder ohne Ermahnung, wenn eine Person am Veranstaltungsort oder in der Aula
+                weder Teilnehmer einer Veranstaltung ist noch ein begründetes Anliegen hat. Anwesende Lehrkräfte und bei
+                Bedarf die SL unterstützen die ATec bei der Durchsetzung.
             </v-expansion-panel-text>
         </v-expansion-panel>
 
         <v-expansion-panel title="4. Pflichten der ATec">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
 
         <v-expansion-panel title="5. Anmeldungen von Veranstaltungen">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
 
         <v-expansion-panel title="6. Kommunikation">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
 
         <v-expansion-panel title="7. Ablauf von Veranstaltungen">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
 
         <v-expansion-panel title="8. Ordnung auf Veranstaltungen">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
         <v-expansion-panel title="9. Ordnung nach Veranstaltungen">
             <v-expansion-panel-text>
-                
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
         <v-expansion-panel title="10. Ordnung in der Aula">
             <v-expansion-panel-text>
-               
+
             </v-expansion-panel-text>
         </v-expansion-panel>
 
@@ -84,9 +103,9 @@ export default {
         </v-expansion-panel>
 
         <v-expansion-panel title="42. Verweigerung von Veranstaltungen">
-        <v-expansion-panel-text>
+            <v-expansion-panel-text>
 
-        </v-expansion-panel-text>
+            </v-expansion-panel-text>
     </v-expansion-panel>
 </v-expansion-panels></template>
 
