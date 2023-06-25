7c87a1ec0fae8dc8b42adf6d568b10a847922456
Barsch2006 <cchr98756@gmail.com>
2023-06-25 19:29:35 +0200

    add try-catch

--- backend/src/Auth/auth.ts
+++ backend/src/Auth/auth.ts
@@ -7,170 +7,191 @@ import { sign, verify } from "jsonwebtoken";
 import { randomBytes } from "crypto";
 
 interface ISession {
-    userId: ObjectId;
-    token: string;
-    expires: number;
+  userId: ObjectId;
+  token: string;
+  expires: number;
 }
 
 declare global {
-    namespace Express {
-        interface Request {
-            auth?: {
-                user?: WithId<IUser>;
-                authenticated: boolean;
-            }
-        }
+  namespace Express {
+    interface Request {
+      auth?: {
+        user?: WithId<IUser>;
+        authenticated: boolean;
+      };
     }
+  }
 }
 
 export default function auth(db: Db): Router {
-    const router = Router();
-    const userCollection = db.collection<IUser>("users");
-    const sessions = db.collection<ISession>("sessions");
+  const router = Router();
+  const userCollection = db.collection<IUser>("users");
+  const sessions = db.collection<ISession>("sessions");
 
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET not set");
+  }
+
+  router.post("/login", async (req, res) => {
     if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET not set");
+      throw new Error("JWT_SECRET not set");
     }
 
-    router.post("/login", async (req, res) => {
-        if (!process.env.JWT_SECRET) {
-            throw new Error("JWT_SECRET not set");
-        }
+    try {
+      // check if body includes username + password
+      if (!req.body || !req.body.username || !req.body.password) {
+        res.status(400).send("Bad Request");
+        return;
+      }
 
-        // check if body includes username + password
-        if (!req.body || !req.body.username || !req.body.password) {
-            res.status(400).send("Bad Request");
-            return;
-        }
+      // get the user from the database
+      const user = await userCollection.findOne({ username: req.body.username });
 
-        // get the user from the database
-        const user = await userCollection.findOne({ username: req.body.username });
+      // compare passwords with bcrypt
 
-        // compare passwords with bcrypt
-
-        if (!user) {
-            res.status(401).send("Unauthorized");
-            return;
-        }
-
-        if (await compare(req.body.password, user.password)) {
-
-            // create a token
-            let token = randomBytes(32).toString("base64");
-
-            // sign token
-            let signed = sign({
-                userId: user._id.toHexString(),
-                token
-            }, process.env.JWT_SECRET, {
-                expiresIn: "2h"
-            });
-
-            // set cookie
-            res.cookie("token", signed, {
-                expires: new Date(Date.now() + 1000 * 60 * 60),
-                sameSite: "none",
-                secure: true,
-                httpOnly: true,
-                maxAge: 1000 * 60 * 60,
-            });
-            console.log("Cookie Set", signed);
-
-            // save token in database
-
-            await sessions.insertOne({
-                userId: user._id,
-                token,
-                expires: Date.now() + 1000 * 60 * 60
-            });
-
-            res.end("OK");
-            return;
-        }
-
+      if (!user) {
         res.status(401).send("Unauthorized");
-    });
+        return;
+      }
 
+      if (await compare(req.body.password, user.password)) {
+        try {
+          // create a token
+          let token = randomBytes(32).toString("base64");
 
-    router.use(async (req, _, next) => {
-        if (req.cookies.token) {
-            // check if token is valid and in db
-            if (!process.env.JWT_SECRET) {
-                throw new Error("JWT_SECRET not set");
+          // sign token
+          let signed = sign(
+            {
+              userId: user._id.toHexString(),
+              token,
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "2h",
             }
-            try {
-                const data = verify(req.cookies.token, process.env.JWT_SECRET, {
-                    ignoreExpiration: true,
-                }) as any;
+          );
 
-                // check if data is in db
-                const session = await sessions.findOne({
-                    userId: new ObjectId(data.userId),
-                    token: data.token
-                });
+          // set cookie
+          res.cookie("token", signed, {
+            expires: new Date(Date.now() + 1000 * 60 * 60),
+            sameSite: "none",
+            secure: true,
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60,
+          });
+          console.log("Cookie Set", signed);
 
-                if (session) {
-                    req.auth = {
-                        authenticated: true,
-                        user: await userCollection.findOne({
-                            _id: new ObjectId(data.userId)
-                        }) ?? undefined,
-                    }
-                    next();
-                    return;
-                }
-            } catch (e) {
-                req.auth = {
-                    authenticated: false,
-                    user: undefined,
-                }
-                next();
-                return;
-            }
+          // save token in database
 
+          await sessions.insertOne({
+            userId: user._id,
+            token,
+            expires: Date.now() + 1000 * 60 * 60,
+          });
+
+          res.end("OK");
+          return;
+        } catch (error) {
+          console.error("Error creating and saving token:", error);
+          res.status(500).send("Internal Server Error");
+          return;
         }
+      }
 
-        req.auth = {
-            authenticated: false,
-            user: undefined,
+      res.status(401).send("Unauthorized");
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  router.use(async (req, _, next) => {
+    try {
+      if (req.cookies.token) {
+        // check if token is valid and in db
+        if (!process.env.JWT_SECRET) {
+          throw new Error("JWT_SECRET not set");
         }
+        try {
+          const data = verify(req.cookies.token, process.env.JWT_SECRET, {
+            ignoreExpiration: true,
+          }) as any;
 
-        next();
+          // check if data is in db
+          const session = await sessions.findOne({
+            userId: new ObjectId(data.userId),
+            token: data.token,
+          });
 
-    });
-
-    router.get("/logout", async (req, res) => {
-        if (!req.auth?.authenticated) {
-            res.status(401).send("Unauthorized");
+          if (session) {
+            req.auth = {
+              authenticated: true,
+              user:
+                (await userCollection.findOne({
+                  _id: new ObjectId(data.userId),
+                })) ?? undefined,
+            };
+            next();
             return;
+          }
+        } catch (e) {
+          console.error("Error verifying token:", e);
         }
-        if (!req.auth?.user) {
-            res.status(400).send("Bad Request");
-            return;
-        }
+      }
 
-        // delete session from database
-        await sessions.deleteMany({
-            userId: req.auth.user._id
-        });
+      req.auth = {
+        authenticated: false,
+        user: undefined,
+      };
 
-        // delete cookie
-        res.clearCookie("token");
+      next();
+    } catch (error) {
+      console.error("Error during authentication:", error);
+      req.auth = {
+        authenticated: false,
+        user: undefined,
+      };
+      next();
+    }
+  });
 
-        res.status(200).send("OK");
+  router.get("/logout", async (req, res) => {
+    try {
+      if (!req.auth?.authenticated) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      if (!req.auth?.user) {
+        res.status(400).send("Bad Request");
+        return;
+      }
 
-    });
+      // delete session from database
+      await sessions.deleteMany({
+        userId: req.auth.user._id,
+      });
 
-    setInterval(async () => {
+      // delete cookie
+      res.clearCookie("token");
 
-        await sessions.deleteMany({
-            expires: {
-                $lt: Date.now()
-            }
-        });
+      res.status(200).send("OK");
+    } catch (error) {
+      console.error("Error during logout:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 
-    }, 1000 * 5 * 60);
+  setInterval(async () => {
+    try {
+      await sessions.deleteMany({
+        expires: {
+          $lt: Date.now(),
+        },
+      });
+    } catch (error) {
+      console.error("Error cleaning expired sessions:", error);
+    }
+  }, 1000 * 5 * 60);
 
-    return router;
-
+  return router;
 }
--- backend/src/Bot/Bot.ts
+++ backend/src/Bot/Bot.ts
@@ -6,62 +6,64 @@ import IEvent from './../Event/event';
 import buildEventEmbed from './Embeds/buildEventEmbed';
 
 class ATecBot extends Client {
-    private applicationId: string;
-    event_channel: string;
-    default_channel: string;
-    db: Db;
+  private applicationId: string;
+  event_channel: string;
+  default_channel: string;
+  db: Db;
 
-    constructor(token: string, applicationId: string, db: Db, options: { default_channel: string, event_channel: string }) {
-        super({
-            intents: [
-                IntentsBitField.Flags.Guilds,
-            ]
-        })
-        this.default_channel = options.default_channel;
-        this.event_channel = options.event_channel;
-        this.applicationId = applicationId;
-        this.db = db;
-        this.login(token).then(() => {
-            console.info("INFO", "Discord Bot is ready!");
-        });
-        interactionCreate(this, this.db);
-        registerCommands(this, token, this.applicationId);
-    }
+  constructor(token: string, applicationId: string, db: Db, options: { default_channel: string, event_channel: string }) {
+    super({
+      intents: [
+        IntentsBitField.Flags.Guilds,
+      ]
+    });
 
-    async sendStringMessage(message: string) {
-        const channel = await this.channels.fetch(this.default_channel);
-        if (!channel || channel.type !== ChannelType.GuildText) {
-            throw new Error("Text-Channel not found!");
-        }
-        await channel.send(message);
-    }
+    this.default_channel = options.default_channel;
+    this.event_channel = options.event_channel;
+    this.applicationId = applicationId;
+    this.db = db;
 
-    async sendEventForm(event: WithId<IEvent>) {
-        const channel = await this.channels.fetch(this.event_channel);
-        if (!channel || channel.type !== ChannelType.GuildText) {
-            throw new Error("Text-Channel not found!");
-        }
-        const message = await channel.send({
-            components: [
-                new ActionRowBuilder<ButtonBuilder>().addComponents(
-                    new ButtonBuilder()
-                        .setLabel("Übernehmen")
-                        .setEmoji('🛠️')
-                        .setCustomId('event-participate')
-                        .setStyle(ButtonStyle.Primary),
-                    // update button
-                    new ButtonBuilder()
-                        .setEmoji('🔄')
-                        .setCustomId('event-update')
-                        .setStyle(ButtonStyle.Secondary),
-                )
-            ],
-            embeds: [
-                await buildEventEmbed(this, event, this.db)
-            ]
-        });
-        return message.id;
+    this.login(token).then(() => {
+      console.info("INFO", "Discord Bot is ready!");
+    });
+
+    interactionCreate(this, this.db);
+    registerCommands(this, token, this.applicationId);
+  }
+
+  async sendStringMessage(message: string) {
+    const channel = await this.channels.fetch(this.default_channel);
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error("Text-Channel not found!");
     }
+    await channel.send(message);
+  }
+
+  async sendEventForm(event: WithId<IEvent>) {
+    const channel = await this.channels.fetch(this.event_channel);
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error("Text-Channel not found!");
+    }
+    const message = await channel.send({
+      components: [
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          new ButtonBuilder()
+            .setLabel("Übernehmen")
+            .setEmoji('🛠️')
+            .setCustomId('event-participate')
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setEmoji('🔄')
+            .setCustomId('event-update')
+            .setStyle(ButtonStyle.Secondary),
+        )
+      ],
+      embeds: [
+        await buildEventEmbed(this, event, this.db)
+      ]
+    });
+    return message.id;
+  }
 }
 
 export default ATecBot;
