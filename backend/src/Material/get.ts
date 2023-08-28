c261667872ecea52b08b4a5572dce5fd716f9bef
Barsch2006 <christian0511@gmx.de>
2023-08-28 21:16:10 +0200

    get all items

--- backend/src/Material/get.ts
+++ backend/src/Material/get.ts
@@ -1,10 +1,75 @@
 import { Router } from "express";
-import { Db } from "mongodb";
+import { Db, ObjectId } from "mongodb";
+import IItem from "./types/item";
 
 export default function getMaterial(db: Db): Router {
   const router = Router();
 
-  // todo
+  const itemCollection = db.collection<IItem>("items");
+
+  router.get("/material/items", async (_req, res) => {
+    const items = await itemCollection.find().toArray();
+    res.json(items);
+  });
+
+  router.get("/material/items/:id", async (req, res) => {
+    const item = await itemCollection.findOne({
+      _id: new ObjectId(req.params.id),
+    });
+
+    if (!item) {
+      res.status(404).json({
+        error: "Item not found",
+      });
+      return;
+    }
+
+    res.json(item);
+  });
+
+  router.get("/material/items/:code", async (req, res) => {
+    const item = await itemCollection.findOne({
+      code: req.params.code,
+    });
+
+    if (!item) {
+      res.status(404).json({
+        error: "Item not found",
+      });
+      return;
+    }
+
+    res.json(item);
+  });
+
+  router.get("/material/items/:category", async (req, res) => {
+    // check if category is valid
+    if (
+      req.params.category !== "cabel" &&
+      req.params.category !== "adapter" &&
+      req.params.category !== "sound" &&
+      req.params.category !== "light" &&
+      req.params.category !== "other"
+    ) {
+      res.status(404).json({
+        error: "Category not found",
+      });
+      return;
+    }
+
+    const items = await itemCollection.find({
+      category: req.params.category,
+    });
+
+    if (!items) {
+      res.status(404).json({
+        error: "Item not found",
+      });
+      return;
+    }
+
+    res.json(items);
+  });
 
   return router;
 }
--- backend/src/Material/types/item.ts
+++ backend/src/Material/types/item.ts
@@ -2,10 +2,13 @@ import { Checklist } from "./checklist";
 
 export type ItemCondition = "fine" | "defect" | "broken";
 
+export type ItemCategory = "cabel" | "adapter" | "sound" | "light" | "other";
+
 export default interface IItem {
   name: string;
   condition: ItemCondition;
   take_checklist: Checklist;
   back_checklist: Checklist;
   location: string;
+  category: ItemCategory;
 }
