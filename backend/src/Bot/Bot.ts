09188476e43fdcfb1343489be20a895976f47b8d
Barsch2006 <christian0511@gmx.de>
2023-08-28 19:46:23 +0200

    add a button to cancel an event participation

--- backend/src/Bot/Bot.ts
+++ backend/src/Bot/Bot.ts
@@ -1,73 +1,89 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, Client, IntentsBitField } from 'discord.js';
-import interactionCreate from './listeners/interactionCreate';
-import registerCommands from './actions/registerCommands';
-import { Db, WithId } from 'mongodb';
-import IEvent from './../Event/event';
-import buildEventEmbed from './Embeds/buildEventEmbed';
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+  Client,
+  IntentsBitField,
+} from "discord.js";
+import interactionCreate from "./listeners/interactionCreate";
+import registerCommands from "./actions/registerCommands";
+import { Db, WithId } from "mongodb";
+import IEvent from "./../Event/event";
+import buildEventEmbed from "./Embeds/buildEventEmbed";
 
 class ATecBot extends Client {
-    private applicationId: string;
-    event_channel: string;
-    default_channel: string;
-    db: Db;
+  private applicationId: string;
+  event_channel: string;
+  default_channel: string;
+  db: Db;
 
-    constructor(token: string, applicationId: string, db: Db, options: { default_channel: string, event_channel: string }) {
-        super({
-            intents: [
-                IntentsBitField.Flags.Guilds,
-            ]
-        })
-        this.default_channel = options.default_channel;
-        this.event_channel = options.event_channel;
-        this.applicationId = applicationId;
-        this.db = db;
-        this.login(token).then(() => {
-            console.info("INFO", "Discord Bot is ready!");
-        });
-        interactionCreate(this, this.db);
-        registerCommands(this, token, this.applicationId);
-    }
+  constructor(
+    token: string,
+    applicationId: string,
+    db: Db,
+    options: { default_channel: string; event_channel: string }
+  ) {
+    super({
+      intents: [IntentsBitField.Flags.Guilds],
+    });
+    this.default_channel = options.default_channel;
+    this.event_channel = options.event_channel;
+    this.applicationId = applicationId;
+    this.db = db;
+    this.login(token).then(() => {
+      console.info("INFO", "Discord Bot is ready!");
+    });
+    interactionCreate(this, this.db);
+    registerCommands(this, token, this.applicationId);
+  }
 
-    async sendStringMessage(message: string) {
-        const channel = await this.channels.fetch(this.default_channel);
-        if (!channel || channel.type !== ChannelType.GuildText) {
-            throw new Error("Text-Channel not found!");
-        }
-        await channel.send(message);
+  async sendStringMessage(message: string) {
+    const channel = await this.channels.fetch(this.default_channel);
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error("Text-Channel not found!");
     }
+    await channel.send(message);
+  }
 
-    async sendEventForm(event: WithId<IEvent>) {
-        const channel = await this.channels.fetch(this.event_channel);
-        if (!channel || channel.type !== ChannelType.GuildText) {
-            throw new Error("Text-Channel not found!");
-        }
-        const message = await channel.send({
-            components: [
-                new ActionRowBuilder<ButtonBuilder>().addComponents(
-                    new ButtonBuilder()
-                        .setLabel("Übernehmen")
-                        .setEmoji('🛠️')
-                        .setCustomId('event-participate')
-                        .setStyle(ButtonStyle.Primary),
-                    // update button
-                    new ButtonBuilder()
-                        .setEmoji('🔄')
-                        .setCustomId('event-update')
-                        .setStyle(ButtonStyle.Secondary),
-                )
-            ],
-            embeds: [
-                await buildEventEmbed(this, event, this.db)
-            ]
-        });
-        // create a thread with the title under the message and send the same message as before
-        await message.startThread({
-            name: event.title,
-            autoArchiveDuration: 1440,
-            reason: "new event thread"
-        });
-        return message.id;
+  async sendEventForm(event: WithId<IEvent>) {
+    const channel = await this.channels.fetch(this.event_channel);
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error("Text-Channel not found!");
     }
+    const message = await channel.send({
+      components: [buildEventMessageComponents()],
+      embeds: [await buildEventEmbed(this, event, this.db)],
+    });
+    // create a thread with the title under the message and send the same message as before
+    await message.startThread({
+      name: event.title,
+      autoArchiveDuration: 1440,
+      reason: "new event thread",
+    });
+    return message.id;
+  }
+}
+
+function buildEventMessageComponents() {
+  return new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setLabel("Übernehmen")
+      .setEmoji("🛠️")
+      .setCustomId("event-participate")
+      .setStyle(ButtonStyle.Success),
+    new ButtonBuilder()
+      .setLabel("Doch nicht")
+      .setEmoji("❌")
+      .setCustomId("event-cancel")
+      .setStyle(ButtonStyle.Secondary),
+    // update button
+    new ButtonBuilder()
+      .setEmoji("🔄")
+      .setCustomId("event-update")
+      .setStyle(ButtonStyle.Primary)
+  );
 }
 
 export default ATecBot;
+export { buildEventMessageComponents };
--- backend/src/Bot/listeners/interactionCreate.ts
+++ backend/src/Bot/listeners/interactionCreate.ts
@@ -1,101 +1,202 @@
-import userinfo from '../actions/userinfo'
-import register from '../actions/register'
+import userinfo from "../actions/userinfo";
+import register from "../actions/register";
 
 import {
-    CommandInteraction, Client, Interaction,
-    ButtonInteraction,
-} from 'discord.js'
-import { Db } from 'mongodb'
-import IEvent from '../../Event/event'
-import IUser from '../../Auth/user'
-import buildEventEmbed from '../Embeds/buildEventEmbed'
+  CommandInteraction,
+  Client,
+  Interaction,
+  ButtonInteraction,
+} from "discord.js";
+import { Db } from "mongodb";
+import IEvent from "../../Event/event";
+import IUser from "../../Auth/user";
+import buildEventEmbed from "../Embeds/buildEventEmbed";
+import { buildEventMessageComponents } from "../Bot";
 
 export default (client: Client, db: Db): void => {
-    client.on('interactionCreate', async (interaction: Interaction) => {
-        if (interaction.isCommand()) {
-            await handleSlashCommand(interaction, db)
-        }
-        if (interaction.isButton()) {
-            await handleButtonInteraction(client, interaction, db)
-        }
-    })
+  client.on("interactionCreate", async (interaction: Interaction) => {
+    if (interaction.isCommand()) {
+      await handleSlashCommand(interaction, db);
+    }
+    if (interaction.isButton()) {
+      await handleButtonInteraction(client, interaction, db);
+    }
+  });
+};
+
+const handleSlashCommand = async (
+  interaction: CommandInteraction,
+  db: Db
+): Promise<void> => {
+  switch (interaction.commandName) {
+    case "userinfo":
+      await userinfo(interaction, db);
+      break;
+    case "register":
+      await register(interaction, db);
+      break;
+  }
+};
+
+const handleButtonInteraction = async (
+  client: Client,
+  interaction: ButtonInteraction,
+  db: Db
+): Promise<void> => {
+  if (interaction.customId === "event-participate") {
+    await newParticipant(client, interaction, db);
+  }
+  if (interaction.customId === "event-update") {
+    await updateEventMessage(interaction, db);
+  }
+  if (interaction.customId === "event-cancel") {
+    await cancelEvent(interaction, db);
+  }
+};
+
+async function newParticipant(
+  client: Client,
+  interaction: ButtonInteraction,
+  db: Db
+) {
+  const event_id = interaction.message.id;
+  const event_obj = await db
+    .collection<IEvent>("events")
+    .findOne({ discordMessageId: { $eq: event_id } });
+  if (!event_obj) {
+    await interaction.reply({
+      content: "Fehler. Event nicht gefunden.",
+      ephemeral: true,
+    });
+    return;
+  }
+  // get the user by username
+  const user = await db
+    .collection<IUser>("users")
+    .findOne({ dId: interaction.user.id });
+
+  if (!user) {
+    await interaction.reply({
+      content: "Fehler. User nicht gefunden.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  event_obj.participants = event_obj.participants ?? [];
+
+  // check if users id is in the participants list of the event
+  if (event_obj.participants.some((id) => id.equals(user._id))) {
+    await interaction.reply({
+      content: "Du übernimmst diese Veranstaltung bereits",
+      ephemeral: true,
+    });
+    return;
+  }
+  await db.collection<IEvent>("events").updateOne(
+    { _id: event_obj._id },
+    {
+      $push: {
+        participants: user._id,
+      },
+    }
+  );
+  await interaction.reply({
+    content: "Du wurdest in die Veranstaltung eingetragen",
+    ephemeral: true,
+  });
+
+  // update the event message
+  if (interaction.message.editable) {
+    await interaction.message.edit({
+      embeds: [await buildEventEmbed(client, event_obj, db)],
+    });
+  }
 }
-
-const handleSlashCommand = async (interaction: CommandInteraction, db: Db): Promise<void> => {
-    switch (interaction.commandName) {
-        case 'userinfo':
-            await userinfo(interaction, db);
-            break;
-        case 'register':
-            await register(interaction, db);
-            break;
-    }
-}
-
-const handleButtonInteraction = async (client: Client, interaction: ButtonInteraction, db: Db): Promise<void> => {
-    if (interaction.customId === 'event-participate') {
-        await newParticipant(client, interaction, db)
-    }
-    if (interaction.customId === 'event-update') {
-        await updateEventMessage(interaction, db)
-    }
-}
-
-async function newParticipant(client: Client, interaction: ButtonInteraction, db: Db) {
-    const event_id = interaction.message.id
-    const event_obj = await db.collection<IEvent>('events').findOne({ discordMessageId: { $eq: event_id } })
-    if (!event_obj) {
-        await interaction.reply({ content: 'Fehler. Event nicht gefunden.', ephemeral: true })
-        return
-    }
-    // get the user by username
-    const user = await db.collection<IUser>('users').findOne({ dId: interaction.user.id })
-
-    if (!user) {
-        await interaction.reply({ content: 'Fehler. User nicht gefunden.', ephemeral: true })
-        return
-    }
-
-    event_obj.participants = event_obj.participants ?? [];
-
-    // check if users id is in the participants list of the event
-    if (event_obj.participants.some((id) => id.equals(user._id))) {
-        await interaction.reply({ content: 'Du übernimmst diese Veranstaltung bereits', ephemeral: true })
-        return;
-    }
-    await db.collection<IEvent>('events').updateOne({ _id: event_obj._id }, {
-        $push: {
-            participants: user._id
-        }
-    })
-    await interaction.reply({ content: 'Du wurdest in die Veranstaltung eingetragen', ephemeral: true })
-
-    // update the event message
-    if (interaction.message.editable) {
-        await interaction.message.edit({
-            embeds: [
-                await buildEventEmbed(client, event_obj, db)
-            ]
-        })
-    }
-}
-
 
 async function updateEventMessage(interaction: ButtonInteraction, db: Db) {
-    const messageId = interaction.message.id;
-    const event = await db.collection<IEvent>('events').findOne({ discordMessageId: { $eq: messageId } });
-    if (!event) {
-        await interaction.reply({ content: 'Fehler. Event nicht gefunden.', ephemeral: true })
-        return
+  const messageId = interaction.message.id;
+  const event = await db
+    .collection<IEvent>("events")
+    .findOne({ discordMessageId: { $eq: messageId } });
+  if (!event) {
+    await interaction.reply({
+      content: "Fehler. Event nicht gefunden.",
+      ephemeral: true,
+    });
+    return;
+  }
+  if (interaction.message.editable) {
+    await interaction.message.edit({
+      embeds: [await buildEventEmbed(interaction.client, event, db)],
+      components: [buildEventMessageComponents()],
+    });
+    await interaction.reply({
+      content: "Event Nachricht aktualisiert",
+      ephemeral: true,
+    });
+  } else {
+    await interaction.reply({
+      content: "Fehler. Event Nachricht nicht editierbar.",
+      ephemeral: true,
+    });
+  }
+}
+
+async function cancelEvent(interaction: ButtonInteraction, db: Db) {
+  // remove the interactions participant from the event
+  const event_id = interaction.message.id;
+  const event_obj = await db
+    .collection<IEvent>("events")
+    .findOne({ discordMessageId: { $eq: event_id } });
+  if (!event_obj) {
+    await interaction.reply({
+      content: "Fehler. Event nicht gefunden.",
+      ephemeral: true,
+    });
+    return;
+  }
+  // get the user by username
+  const user = await db
+    .collection<IUser>("users")
+    .findOne({ dId: interaction.user.id });
+
+  if (!user) {
+    await interaction.reply({
+      content: "Fehler. User nicht gefunden.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  event_obj.participants = event_obj.participants ?? [];
+
+  // check if users id is in the participants list of the event
+  if (!event_obj.participants.some((id) => id.equals(user._id))) {
+    await interaction.reply({
+      content: "Du bist nicht in dieser Veranstaltung",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  await db.collection<IEvent>("events").updateOne(
+    { _id: event_obj._id },
+    {
+      $pull: {
+        participants: user._id,
+      },
     }
-    if (interaction.message.editable) {
-        await interaction.message.edit({
-            embeds: [
-                await buildEventEmbed(interaction.client, event, db)
-            ]
-        })
-        await interaction.reply({ content: 'Event Nachricht aktualisiert', ephemeral: true })
-    } else {
-        await interaction.reply({ content: 'Fehler. Event Nachricht nicht editierbar.', ephemeral: true })
-    }
+  );
+  await interaction.reply({
+    content: "Du wurdest aus der Veranstaltung entfernt",
+    ephemeral: true,
+  });
+
+  // update the event message
+  if (interaction.message.editable) {
+    await interaction.message.edit({
+      embeds: [await buildEventEmbed(interaction.client, event_obj, db)],
+    });
+  }
 }
